b27a9fe4ebffde03f6fc7db935da6ac58b58c33a
Mohamed Shakir <mshak084@uottawa.ca>
2024-03-11 19:42:57 -0400

    attempting to fix middleware crashing edge

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,10 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { cookies } from "next/headers";
-import { getSession } from "@/lib";
-import { decrypt, encrpyt } from "@/lib";
 
 export async function middleware(request: NextRequest) {
-	const session = cookies().get("session")?.value;
-
-	if (!session) return;
-
-	if (request.nextUrl.pathname === "/Bookings" && !session) {
-		return NextResponse.redirect(new URL("/Login", request.nextUrl));
-	}
+	// const session = cookies().get("session")?.value;
+	// if (!session) return;
+	// if (request.nextUrl.pathname === "/Bookings" && !session) {
+	// 	return NextResponse.redirect(new URL("/Login", request.nextUrl));
+	// }
 }
