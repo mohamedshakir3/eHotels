b83ed465b6220507923c1b5aadd6aed17657c62a
Mohamed Shakir <mshak084@uottawa.ca>
2024-03-11 20:00:12 -0400

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import { NextResponse, type NextRequest } from "next/server";
 import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-	// const session = cookies().get("session")?.value;
-	// if (!session) return;
-	// if (request.nextUrl.pathname === "/Bookings" && !session) {
-	// 	return NextResponse.redirect(new URL("/Login", request.nextUrl));
-	// }
+	const session = cookies().get("session")?.value;
+	if (!session) return;
+	if (request.nextUrl.pathname === "/Bookings" && !session) {
+		return NextResponse.redirect(new URL("/Login", request.nextUrl));
+	}
 }
